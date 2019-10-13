7ca8809eb5e1bea19daef02f16dc3c49ec8d1cd8
supriya  yerramilli <supriyayerramilli@supriyas-MacBook-Air.local>
2019-10-13 18:53:00 -0400

    added html

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -7,6 +7,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoComponent implements OnInit {
 
+  mockData: any = [
+    {
+      id: '1',
+      title: 'Pack lunch for the week',
+      done: false,
+      date: new Date()
+    },
+    {
+      id: '2',
+      title: 'Prepare for frontend interview',
+      done: false,
+      date: new Date()
+    },
+    {
+      id: '3',
+      title: 'Prepare for backend interview',
+      done: false,
+      date: new Date()
+    },
+    {
+      id: '4',
+      title: 'Build simple project in angular',
+      done: false,
+      date: new Date()
+    },
+  ];
+
+//properties to show hide edit form, set updated value and id.
+
+show: boolean = false;
+value: string;
+id: number;
+
   constructor() { }
 
   ngOnInit() {
